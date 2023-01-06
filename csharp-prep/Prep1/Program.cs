2feb5beafa88c3898cc4ccb177ac80debc194441
emfernandezv <36644789+emfernandezv@users.noreply.github.com>
2023-01-05 23:35:27 -0700

    Prep1 & 2

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,19 @@
 using System;
 
+
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        //declare
+        string firstName, lastName;
+
+        Console.WriteLine("What is your first name?");
+        firstName = Console.ReadLine();
+        Console.WriteLine("What is your last name?");
+        lastName = Console.ReadLine();
+
+        Console.WriteLine($"You name is {lastName}, {firstName} {lastName}.");
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,58 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        
+        //declare
+        double gradePerc;
+        string letter, sign = "";
+
+        //Core Requirements
+
+        Console.WriteLine("Please insert your grade percentage:");
+        gradePerc =  float.Parse(Console.ReadLine());
+
+        if (gradePerc >= 90){
+            letter = "A";
+        } else if (gradePerc >= 80){
+            letter = "B";
+        } else if (gradePerc >= 70){
+            letter = "C";
+        } else if (gradePerc >= 60){
+            letter = "D";
+        } else {
+            letter = "F";
+        };
+
+        Console.WriteLine($"You grade is: {letter}");
+
+        if (gradePerc >= 70){
+            Console.WriteLine("Congratulations, you passed!");
+        } else {
+            Console.WriteLine("Better luck next time. Keep practicing!");
+        };
+
+        //Stretch Challenge
+
+        //Sign determination
+        gradePerc =  Math.Round((gradePerc / 10) %1,1)*10;
+        
+        if (gradePerc >= 7){
+            sign = "+";
+        } else if (gradePerc < 3){
+            sign = "-";
+        };
+
+        //No A+ Determination
+        if (letter == "A" && sign == "+"){
+            sign = "";
+        };
+
+        //No F+ or F- Determination
+        if (letter == "F"){
+            sign = "";
+        }
+
+        Console.WriteLine($"You final grade is: {letter}{sign}");
+
     }
 }
