f1652415ca8b54e0641655102b89c278d1a72881
emfernandezv <36644789+emfernandezv@users.noreply.github.com>
2023-01-20 00:11:23 -0700

    Lesson02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        //setting current job
+        Job job1 = new Job();
+        job1._company = "AdvancedMD";
+        job1._jobTitle = "Interface Delivery Analyst Sr";
+        job1._startYear = 2022;
+        job1._endYear = 2023;
+
+        //setting previous job
+        Job job2 = new Job();
+        job2._company = "Sygnus Consulting";
+        job2._jobTitle = "Application Manager";
+        job2._startYear = 2017;
+        job2._endYear = 2021;
+
+        //setting resumen
+        Resume resume = new Resume();
+        resume._name = "Eduardo Fernandez";
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+
+        // display values
+        resume.Display();
+
+
     }
 }
