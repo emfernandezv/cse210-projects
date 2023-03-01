727bed7f2696483ae64fea8adfe68574d044a6b6
emfernandezv <36644789+emfernandezv@users.noreply.github.com>
2023-02-28 22:55:36 -0700

    Lesson 5

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -3,7 +3,26 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Learning05 World!");
+    {   
+        List<Shape> shapes = new List<Shape>();
+
+        Square testSquare = new Square("White",10);
+        shapes.Add(testSquare);
+        //Console.WriteLine(testSquare.GetColor());
+        //Console.WriteLine(testSquare.GetArea());
+
+        Rectangle testRectangle = new Rectangle("Black",5,10);
+        shapes.Add(testRectangle);
+        //Console.WriteLine(testRectangle.GetColor());
+        //Console.WriteLine(testRectangle.GetArea());
+
+        Circle testCircle = new Circle("Red",10);
+        shapes.Add(testCircle);
+        //Console.WriteLine(testCircle.GetColor());
+        //Console.WriteLine(testCircle.GetArea());
+
+        foreach(Shape s in shapes){
+            Console.WriteLine($"The color is: {s.GetColor()} and the Area is: {s.GetArea()}");
+        }
     }
 }
